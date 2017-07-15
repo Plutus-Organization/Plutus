d1929ddee0c872f4ebd4991b36e0c73d72f9434b
Jason Zhang <jszhang@linkedin.com>
2017-07-15 05:14:13 -0700

    get_data endpoint

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import datetime
+
 from flask import Flask, jsonify, render_template, request
+
+from . import stock
+
 app = Flask(__name__)
+
+
+@app.route('/stock/get_data')
+def get_data():
+    req = request.json
+    ticker = req['ticker']
+    start_date = req.get('start_date', '')
+    end_date = req.get('end_date', '')
+
+    timestamps, prices = stock.get_data(ticker, start_date, end_date)
+    timestamps = [time.strftime('%Y-%m-%d %H:%M:%S') for time in timestamps]
+    prices = list(prices)
+    return jsonify(timestamps=timestamps, prices=prices)
 
 
 @app.route('/')
