b8b6bb6e25fe195608a69e0ae9859eae7d1f783b
SirjanK <sxkafle12@gmail.com>
2017-07-15 06:31:55 -0700

    Add endpoint for retrieving article based on stock name

--- articles.py
+++ articles.py
@@ -8,24 +8,20 @@ from static.credentials import *
 
 
 class StockArticle:
-    def __init__(self, stock_symbol, article_url, article_summary):
+    def __init__(self, stock_name, article_url, article_summary):
         """
         Initializes a Stock Article.
-        :param stock_symbol:    symbol for the stock the article is associated with.
+        :param stock_name:      name for the stock the article is associated with.
         :param article_url:     url of the article.
         :param article_summary: summary of the article.
         """
-        self.stock_symbol = stock_symbol
+        self.stock_name = stock_name
         self.article_url = article_url
         self.article_summary = article_summary
 
-    #TODO: store article into db
-    def store_article_into_db(self):
-        pass
-
 
 class RelevantArticlesSource:
-    def __init__(self, num_articles, num_sentences):
+    def __init__(self, num_sentences):
         """
         Initializes the Source class.
         :param num_sentences: number of sentences returned by the SMMRY api.
@@ -33,27 +29,24 @@ class RelevantArticlesSource:
         self.db = {}
         self.num_sentences = num_sentences
 
-    def retrieve_topmost_article(self, stock_symbol, stock_name):
+    def retrieve_topmost_article(self, stock_name):
         """
         Retrieves the topmost article about the stock.
-        :param stock_symbol: stock symbol.
         :param stock_name:   stock name.
         :return:             a StockArticle object.
         """
-        if stock_symbol in self.db:
-            return self.db[stock_symbol]
-        return self.retrieve_topmost_article_new(stock_symbol, stock_name)
+        if stock_name in self.db:
+            return self.db[stock_name]
+        return self.retrieve_topmost_article_new(stock_name)
 
-    def retrieve_topmost_article_new(self, stock_symbol, stock_name):
+    def retrieve_topmost_article_new(self, stock_name):
         """
         Retrieves the topmost article about the stock, but solely through the usage of the webhose API. This
         does not involve checking the database for an already existing article for the stock.
-        :param stock_symbol: stock symbol.
         :param stock_name:   stock name.
         :return:             a StockArticle object.
         """
         webhoseio.config(token=webhose_api_key)
-        stock_name = stock_name.lower()
         filters = {'language': 'english', 'text': stock_name, 'site_type': 'news', 'site_category': 'finance', 'thread.title': stock_name}
         query_result = webhoseio.query('filterWebContent', filters)
         stock_posts = query_result['posts']
@@ -63,14 +56,14 @@ class RelevantArticlesSource:
         article_url = article_info['url']
         article_summary = self.summarize_article(article_url)
 
-        return StockArticle(stock_symbol, article_url, article_summary)
+        return StockArticle(stock_name, article_url, article_summary)
 
     def add_article_to_db(self, stock_article):
         """
         Adds a stock article into the database.
         :param stock_article: stock article to be added.
         """
-        self.db[stock_article.stock_symbol] = stock_article
+        self.db[stock_article.stock_name] = stock_article
 
     def summarize_article(self, article_url):
         """
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 from flask import Flask, jsonify, render_template, request
 
 from static import stock
+from articles import *
 
 app = Flask(__name__)
 
@@ -19,6 +20,15 @@ def get_history():
     timestamps = [time.strftime('%Y-%m-%d %H:%M:%S') for time in timestamps]
     prices = list(prices)
     return jsonify(timestamps=timestamps, prices=prices)
+
+
+@app.route('/get_article_from_name', methods=['GET'])
+def get_article_for_stock_name():
+    stock_name = request.args.get('name')
+    article_source_manager = RelevantArticlesSource(2)
+    article = article_source_manager.retrieve_topmost_article(stock_name.lower())
+    article_source_manager.add_article_to_db(article)
+    return jsonify(url=article.article_url, summary=article.article_summary)
 
 
 @app.route('/')
